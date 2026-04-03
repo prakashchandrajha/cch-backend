e21d8caee8d27799de15c80be36738d36ff786c1
ravichanderjha <ravichanderjha@gmail.com>
2026-04-03 10:51:45 +0530

    q

--- src/main/java/com/ncdc/ica/cch/repository/NominationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ncdc.ica.cch.repository;
-
-import com.ncdc.ica.cch.entity.NominationForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NominationRepository extends JpaRepository<NominationForm, Long> {
-}
