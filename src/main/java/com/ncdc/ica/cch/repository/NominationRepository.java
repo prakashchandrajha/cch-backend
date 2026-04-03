f30da53c2402758d839da1e92ab06835a2aedc56
ravichanderjha <ravichanderjha@gmail.com>
2026-04-03 18:26:45 +0530

    remove all nomination crud

--- src/main/java/com/ncdc/ica/cch/repository/NominationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ncdc.ica.cch.repository;
-
-import com.ncdc.ica.cch.entity.Nomination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NominationRepository extends JpaRepository<Nomination, Long> {
-}
