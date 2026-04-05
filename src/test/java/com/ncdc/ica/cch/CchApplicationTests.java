6bf74e503cff2273e10ae1de76ade9ab8cb76657
prakash <prakash@gmail.com>
2026-04-05 15:00:37 +0530

    comment out test

--- src/test/java/com/ncdc/ica/cch/CchApplicationTests.java
+++ src/test/java/com/ncdc/ica/cch/CchApplicationTests.java
@@ -3,7 +3,7 @@ package com.ncdc.ica.cch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class CchApplicationTests {
 
 	@Test
